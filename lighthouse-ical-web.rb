f161c8a90554b282701b3c82170cb7dbf98bb63f
Craig Anderson <craiga@craiga.id.au>
2011-03-04 10:34:09 +1100

    Change so configuration is passed through URL structure

--- lighthouse-ical-web.rb
+++ lighthouse-ical-web.rb
@@ -2,7 +2,19 @@ require 'sinatra'
 require 'lighthouse-ical'
 
 get '/' do
-	Lighthouse.account = 'your-account'
-	Lighthouse.token = 'your-token'
-	LighthouseIcal.create_calendar_for_project_id(123)
+	'TODO: Build home page'
+	# Lighthouse.account = 'your-account'
+	# Lighthouse.token = 'your-token'
+	# LighthouseIcal.create_calendar_for_project_id(123)
+end
+
+
+get %r{^/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)$} do
+	account = params[:captures][0]
+	token = params[:captures][1]
+	projectid = params[:captures][1].to_i
+	
+	Lighthouse.account = account
+	Lighthouse.token = token
+	LighthouseIcal.create_calendar_for_project_id(projectid)
 end
