eb7cdf8399046d0aa1015bb5054a353201d2704c
Craig Anderson <craiga@craiga.id.au>
2011-03-03 17:47:25 +1100

    Adding generic user credentials

--- lighthouse-ical-web.rb
+++ lighthouse-ical-web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'lighthouse-ical'
 
 get '/' do
-	Lighthouse.account = 'sitepoint'
-	Lighthouse.token = '97773cd93c15f8a650d61991b41d1beeec8bd63b'
-	LighthouseIcal.create_calendar_for_project_id(69822)
+	Lighthouse.account = 'your-account'
+	Lighthouse.token = 'your-token'
+	LighthouseIcal.create_calendar_for_project_id(123)
 end
