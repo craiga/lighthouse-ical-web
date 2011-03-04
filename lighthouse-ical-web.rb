cf5e276a7b9103e6d73efcc212a633e99c8eda8b
Craig Anderson <craiga@craiga.id.au>
2011-03-04 11:02:40 +1100

    Fixing project ID

--- lighthouse-ical-web.rb
+++ lighthouse-ical-web.rb
@@ -2,18 +2,13 @@ require 'sinatra'
 require 'lighthouse-ical'
 
 get '/' do
-	'TODO: Build home page<br />'
 	'http://foobar.heroku.com/account/token/projectid'
-	# Lighthouse.account = 'your-account'
-	# Lighthouse.token = 'your-token'
-	# LighthouseIcal.create_calendar_for_project_id(123)
 end
 
-
-get %r{^/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)$} do
+get %r{^/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/(\d+)$} do
 	account = params[:captures][0]
 	token = params[:captures][1]
-	projectid = params[:captures][1].to_i
+	projectid = params[:captures][2].to_i
 	
 	Lighthouse.account = account
 	Lighthouse.token = token
